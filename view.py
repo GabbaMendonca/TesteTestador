cef22fdc98c9cd1cfecb2a4f41609f2878ddd8a7
GabbaMendonca <gabba.kumbuca@gmail.com>
2020-05-16 19:45:38 -0300

    Chamando func no dic menu e alt dic event

--- view.py
+++ view.py
@@ -10,38 +10,50 @@ event['ip'] = '10.10.10.10'
 def c():
     print('Sou a func c')
 
-def b():
-    print('Sou a func b')
-    print('Camando func c')
-    c()
+def terminal_start():
+    controller.terminal_start(event['user'], event['senha'], event['ip'], False)
+
+def select_router_alcatel():
+    controller.select_router_alcatel()
+    
+def router_alcatel_telnet():
+    controller.router_alcatel_telnet()
+    
+def router_alcatel_version():
+    controller.router_alcatel_version()
+    
 
 def a():    
-    opc = input('Opc : ')
+    while True:
+        try:
+            opc = input('Opc : ')
 
-    func = com['menu_inicial'][opc]
-    func()
+            func = com['menu inicial'][opc]
+            func()
+        except KeyError:
+            print('Este não tem ...')
 
-    opc = input('Opc : ')
 
-    func = com['menu_inicial'][opc]
-    func()
 
 
 com = {
-    'menu_inicial': {
-        '1': controller.terminal_start(event['user'], event['senha'], event['ip'], False),
-        '2': controller.select_router_alcatel,
-        '3': controller.router_alcatel_telnet,
-        '4': controller.router_alcatel_version,
-        '5': b,
+    'menu inicial': {
+        '0': exit,
+        '1': terminal_start,
+        '2': select_router_alcatel,
+        '3': router_alcatel_telnet,
+        '4': router_alcatel_version,
+        '5': c,
     }
 }
 
-a()
+def iniciar():
+    event['user'] = input('User : ')
+    event['senha'] = input('Senha : ')
+    event['ip'] = input('IP : ')
+    a()
 
-
-
-
+iniciar()
 
 
 

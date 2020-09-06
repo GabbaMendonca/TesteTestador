347ad51cbede1177bb8929dd7e68b5e7fee769b0
Gabriel Silva Mendonça <gabba@pop-os.localdomain>
2020-09-06 00:23:33 -0300

    :white_check_mark: Adicionado teste para settings

---
+++ test.py
@@ -0,0 +1,59 @@
+import unittest
+
+from settings import Settings
+from iniciar import carregar_dados
+
+class MyTest(unittest.TestCase):
+    def test_settings_server(self):
+        s = Settings()
+        self.assertEqual(s.server.new_server('teste','0.0.0.0'), True)
+        
+        server = s.server.get_server()
+        self.assertEqual(server['teste'], '0.0.0.0')
+        
+        server = s.server.update_server('teste','1.1.1.1')
+        server = s.server.get_server()
+        self.assertEqual(server['teste'], '1.1.1.1')
+
+        server = s.server.deletar_server('teste')
+        server = s.server.get_server()
+        if 'teste' in server:
+            self.assertTrue(False)
+        else:
+            self.assertTrue(True)
+
+        print(' Valor atual no dict server : ', server)
+
+
+    def test_settings_login(self):
+        s = Settings()
+        self.assertEqual(s.login.new_login('usuario','senha'), True)
+        
+        login = s.login.get_login()
+        self.assertEqual(login['usuario'], 'senha')
+
+        login = s.login.update_login('usuario','password')
+        login = s.login.get_login()
+        self.assertEqual(login['usuario'], 'password')
+
+        login = s.login.deletar_login('usuario')
+        login = s.login.get_login()
+        if 'usuario' in login:
+            self.assertTrue(False)
+        else:
+            self.assertTrue(True)
+        
+        print(' Valor atual no dict login : ', login)
+
+    def test_carregar_dados(self):
+        s = Settings()
+        s.server.new_server('teste','0.0.0.0')
+        s.login.new_login('usuario','senha')
+        dados = carregar_dados()
+
+        self.assertEqual(dados , ({'usuario':'senha'}, {'teste':'0.0.0.0'}))
+        print(' Tupla retornada com os dados ususario e senha : ', dados)
+    
+
+if __name__ == '__main__':
+    unittest.main()
